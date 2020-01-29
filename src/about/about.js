6cfa58067d5a30fc9fefe2c5d3d88c523ecf988f
Andrew Thankson <athankso@u.rochester.edu>
2020-01-28 20:11:01 -0500

    more additions to about page

--- src/about/about.js
+++ src/about/about.js
@@ -3,13 +3,9 @@ import hostbackground from "../photos/hostbackground.jpg";
 import "./about.css";
 import Navbar from "../commons/Navbar";
 import FooterPage from "../commons/footer";
-import chef from "../photos/chef.jpg";
-import woman from "../photos/woman.jpg";
 import faces from "../photos/Faces.png";
 import kaixin from "../photos/kaixin.jpg";
-import fquote from "../photos/fquote.png";
 import quotes from "../photos/quotes.png";
-import bquote from "../photos/bquote.png";
 import yawo from "../photos/yawo.png";
 import shawn from "../photos/shawn.png";
 import carolina from "../photos/carolina.png";
@@ -19,67 +15,67 @@ import may from "../photos/may.png";
 class about extends React.Component {
   render() {
     return (
-      <div className="App">
+      <div className="container">
         <Navbar />
 
         <figure className="experience-fig">
           <h2>About</h2>
         </figure>
-        <div>
-          <p>
-            Yolo is a job shadowing marketplace that connects locals and
-            travelers for authentic job shadow experiences. Locals can earn
-            extra income for hosting job shadow experiences that are unique to
-            their culture and tradition. Travelers get to experience working in
-            a new city or a new field for a short period of time (1-3 days).
-            Yolo is a job shadowing marketplace for two parties to exchange work
-            experience, culture and knowledge. 
-          </p>
 
+        <p className="text-left">
+          Yolo is a job shadowing marketplace that connects locals and travelers
+          for authentic job shadow experiences. Locals can earn extra income for
+          hosting job shadow experiences that are unique to their culture and
+          tradition. Travelers get to experience working in a new city or a new
+          field for a short period of time (1-3 days). Yolo is a job shadowing
+          marketplace for two parties to exchange work experience, culture and
+          knowledge. 
+        </p>
+
+        <div className="row">
           <div>
-            <div className="left">
-              <h4>WHY HOST</h4>
+            <h3 className="why-yolo">Why Yolo</h3>
+          </div>
+          <div className="col">
+            <h4>WHY HOST</h4>
 
-              <strong>Demonstrate your values</strong>
-              <p>
-                Show your openness, dedication to diversity & inclusion, and
-                willingness to give back.
-              </p>
+            <strong>Demonstrate your values</strong>
+            <p>
+              Show your openness, dedication to diversity & inclusion, and
+              willingness to give back.
+            </p>
 
-              <strong>Get a new perspective</strong>
-              <p>
-                Infuse your business with a different point of view and
-                innovative ideas.
-              </p>
+            <strong>Get a new perspective</strong>
+            <p>
+              Infuse your business with a different point of view and innovative
+              ideas.
+            </p>
 
-              <strong>Grow your brand</strong>
-              <p>
-                Bring people along on your business journey and create a
-                community of fans and advocates and get rewarded for it.
-              </p>
-            </div>
-
-            <div className="right">
-              <h4>WHY SHADOW</h4>
-              <strong>Learn and grow</strong>
-              <p>
-                Learn directly from experts who can inspire you to pursue your
-                passions.
-              </p>
-              <strong>Try before you commit </strong>
-              <p>
-                Starting or changing career is daunting, we make it easy to
-                sample new industries and companies before committing.
-              </p>
-              <strong>Grow your brand</strong>
-              <p>
-                Connect with inspiring professionals across a variety of
-                industries.
-              </p>
-            </div>
+            <strong>Grow your brand</strong>
+            <p>
+              Bring people along on your business journey and create a community
+              of fans and advocates and get rewarded for it.
+            </p>
           </div>
 
-          <h3 className="why-yolo">Why Yolo</h3>
+          <div className="col">
+            <h4>WHY SHADOW</h4>
+            <strong>Learn and grow</strong>
+            <p>
+              Learn directly from experts who can inspire you to pursue your
+              passions.
+            </p>
+            <strong>Try before you commit </strong>
+            <p>
+              Starting or changing career is daunting, we make it easy to sample
+              new industries and companies before committing.
+            </p>
+            <strong>Grow your brand</strong>
+            <p>
+              Connect with inspiring professionals across a variety of
+              industries.
+            </p>
+          </div>
         </div>
 
         <div className="team">
