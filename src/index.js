4f438ce522040d45f1105465fd292166cf1995a1
Andrew Thankson <athankso@u.rochester.edu>
2020-02-01 11:42:02 -0500

    footer styling with bootstrap. not fully done

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./story/FeatureStory.js";
+import App from "./commons/footer.js";
 import hostExperience from "./hostExperience/hostExperience";
 import about from "./about/about";
 
