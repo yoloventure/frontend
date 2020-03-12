b83918aa4b891cc2f050384290d9bb520d61d21a
Andrew Thankson <athankso@u.rochester.edu>
2020-03-12 11:31:03 -0400

    about page polished; bottom component polished

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-import App from "./App"
-
+import App from "./about/about";
 
 ReactDOM.render(<App />, document.getElementById("root"));
