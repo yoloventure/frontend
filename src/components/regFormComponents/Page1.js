4d031a119e866a83de0e8a5292098cac076f4809
Andrew Thankson <athankso@u.rochester.edu>
2020-04-20 23:59:08 -0400

    regForm page 1 edit and style

--- src/components/regFormComponents/Page1.js
+++ src/components/regFormComponents/Page1.js
@@ -6,6 +6,9 @@ class Page1 extends React.Component {
     this.state = {
       userName: "Full name",
       contact: "Enter your contact information",
+      gender: "female",
+      title: "ex: Recruiter",
+      stage: "career stage",
       company: "example: YoloShadow",
       email: "user@gmail.com",
       phone: "22-22-222",
@@ -41,10 +44,13 @@ class Page1 extends React.Component {
       <div>
         <form onSubmit={this.handleSubmit}>
           <div className="container-fluid">
-            <div className="row">
+            <div className="row mt-1 mb-3">
               <h3>Tell us about what you do.</h3>
             </div>
-            <div className="row">
+            <div className="row mb-1">
+              <div className="col-sm-2">
+                <h6>Personal</h6>
+              </div>
               <div className="col-sm-2">
                 <i className="">insert icon</i>
               </div>
@@ -52,10 +58,17 @@ class Page1 extends React.Component {
               <p>import from LinkedIn</p>
             </div>
             <div className="container-fluid">
-              <div className="row">
+              <div className="row mb-n2">
+                <div className="col ml-n3 ">
+                  <label>Name</label>
+                </div>
+                <div className="col ml-n3">
+                  <label>Email</label>
+                </div>
+              </div>
+              <div className="row ">
                 <div className="col-sm-6">
                   <div className="row">
-                    <label>Name</label>
                     <input
                       type="text"
                       name="userName"
@@ -66,39 +79,37 @@ class Page1 extends React.Component {
                 </div>
                 <div className="col-sm-6">
                   <div className="row">
-                    <label>Contact</label>
-                    <input
-                      name="contact"
-                      value={this.state.contact}
-                      type="text"
-                      onChange={this.handleInputChange}
-                    />
-                  </div>
-                </div>
-              </div>
-
-              <div className="row">
-                <div className="col-sm-6">
-                  <div className="row">
-                    <label>Company</label>
-                    <input
-                      name="company"
-                      value={this.state.company}
-                      type="text"
-                      onChange={this.handleInputChange}
-                    />
-                  </div>
-                </div>
-                <div className="col-sm-6">
-                  <div className="row">
-                    <label>Email</label>
                     <input
                       name="email"
                       value={this.state.email}
                       onChange={this.handleInputChange}
                       type="email"
                     />
-                    <label>Phone</label>
+                  </div>
+                </div>
+              </div>
+              <div className="row mt-3 mb-n2">
+                <div className="col-sm-6 ml-n3">
+                  <label>Gender</label>
+                </div>
+                <div className="col-sm-6">
+                  <label>Phone</label>
+                </div>
+              </div>
+
+              <div className="row">
+                <div className="col-sm-6">
+                  <div className="row">
+                    <input
+                      name="gender"
+                      value={this.state.gender}
+                      type="text"
+                      onChange={this.handleInputChange}
+                    />
+                  </div>
+                </div>
+                <div className="col-sm-6">
+                  <div className="row">
                     <input
                       name="phone"
                       value={this.state.phone}
@@ -108,48 +119,75 @@ class Page1 extends React.Component {
                   </div>
                 </div>
               </div>
-
+              <div className="row mt-3 mb-n2">
+                <label htmlFor="title">Title</label>
+              </div>
               <div className="row">
-                <div className="col-sm-6">
+                <input
+                  type="text"
+                  name="title"
+                  value={this.state.title}
+                  type="text"
+                  onChange={this.handleInputChange}
+                />
+              </div>
+              <div className="row mt-3 mb-n2">
+                <div className="col">
+                  <div className="row mb-n2">
+                    <label htmlFor="">Career Stage</label>
+                  </div>
                   <div className="row">
-                    <label>Company Website/Social Media</label>
                     <input
+                      type="text"
+                      name="stage"
+                      value={this.state.stage}
+                      type="text"
+                      onChange={this.handleInputChange}
+                    />
+                  </div>
+                  <div className="row mt-3 mb-n3">
+                    <label htmlFor="">
+                      <label htmlFor="">Company</label>
+                    </label>
+                  </div>
+                  <div className="row">
+                    <input
+                      type="text"
+                      name="company"
+                      value={this.state.company}
+                      type="text"
+                      onChange={this.handleInputChange}
+                    />
+                  </div>
+                  <div className="row mt-3 mb-n2">
+                    <label>Company website</label>
+                  </div>
+                  <div className="row">
+                    <input
+                      type="text"
                       name="website"
                       value={this.state.website}
                       type="text"
                       onChange={this.handleInputChange}
                     />
                   </div>
+                  <div className="row"></div>
                 </div>
-                <div className="col-sm-6">
-                  <div className="row">
-                    <label>Category</label>
-                    <input
-                      name="category"
-                      value={this.state.category}
-                      type="text"
-                      onChange={this.handleInputChange}
-                    />
-                  </div>
+                <div className="col">
+                  <label htmlFor="">One line to describe what you do</label>
+                  <textarea></textarea>
                 </div>
               </div>
 
-              <div className="row">
-                <div className="col-sm-12">
-                  <div className="row">
-                    <label>One line to describe what you do</label>
-                    <input
-                      name="description"
-                      value={this.state.description}
-                      type="text"
-                      onChange={this.handleInputChange}
-                    />
-                  </div>
+              <div className="row mt-5 mb-4">
+                <div className="col"></div>
+                <div className="col">
+                  <input
+                    class="btn btn-danger"
+                    type="submit"
+                    value="Next Step"
+                  />
                 </div>
-              </div>
-
-              <div className="row submit">
-                <input type="submit" value="Next Step" />
               </div>
             </div>
           </div>
--- src/registration/HostRegister.js
+++ src/registration/HostRegister.js
@@ -14,25 +14,44 @@ import Page7 from "../components/regFormComponents/Page7";
 class HostRegister extends React.Component {
   constructor(props) {
     super(props);
+
     this.state = {
-      counter: 1
+      counter: 1,
+      progress: 0
     };
 
     this.handleSubmit = this.handleSubmit.bind(this);
+    // this.handleProgressBar = this.handleProgressBar.bind(this);
   }
 
   handleSubmit(event) {
+    if (this.state.counter == 1) {
+      this.setState({ progess: 0 });
+    } else if (this.state.counter == 2) {
+      this.setState({ progess: 25 });
+    } else if (this.state.counter == 3) {
+      this.setState({ progess: 50 });
+    } else if (this.state.counter == 4) {
+      this.setState({ progess: 75 });
+    } else if (this.state.counter == 5) {
+      this.setState({ progess: 95 });
+    } else {
+      this.setState({ progess: 100 });
+    }
+
     this.setState({ counter: this.state.counter + 1 });
   }
 
   handlePageRender(counter) {
     if (counter == 1) {
+      console.log(this.props.progress);
       const pageToRender = <Page1 />;
+
       return pageToRender;
     } else if (counter == 2) {
+      // this.setState({ progress: 25 });
       return <Page2 />;
     } else if (counter == 3) {
-      // this.setState({ checkCounter: 4 });
       return <Page3 />;
     } else if (counter == 4) {
       // this.setState({ checkCounter: 5 });
@@ -49,6 +68,10 @@ class HostRegister extends React.Component {
     }
   }
 
+  // handleProgressBar(counter) {
+
+  // }
+
   render() {
     return (
       <div className="container-fluid app">
@@ -56,7 +79,7 @@ class HostRegister extends React.Component {
           <Navbar textColor={"black"} />
         </div>
 
-        <div className="constant">
+        <div className="container mt-5 mb-5">
           <div className="top row">
             <div className="col-md-4">
               <img
@@ -75,13 +98,25 @@ class HostRegister extends React.Component {
           {/*Progress Bar*/}
 
           <div onSubmit={this.handleSubmit}>
-            <div className="row progress">
+            <div className="row mt-5">
               <div className="col-sm-2">
                 <p>PROGRESS</p>
               </div>
-              <div className="progress-bar"></div>
+
+              <div className="col-sm-10 progress">
+                <div
+                  className={"progress-bar w-" + this.state.progress}
+                  role="progressbar"
+                  aria-valuenow="0"
+                  aria-valuemin="0"
+                  aria-valuemax="100"
+                >
+                  {this.state.progress}%
+                </div>
+              </div>
             </div>
             {/*Components go here: replace with components */}
+
             <div className="insert">
               {this.handlePageRender(this.state.counter)}
             </div>
