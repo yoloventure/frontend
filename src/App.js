aa7a6de354b9f976c16b938257c3c8492773c992
Abdul Moid Munawar <amunawar@u.rochester.edu>
2020-04-21 02:53:02 -0400

    making consistent with master

--- src/App.js
+++ src/App.js
@@ -3,71 +3,110 @@ import firebase from "./config/firebase";
 import Homepage from "./home/Homepage";
 import Explore from "./explore/Explore";
 import Login from "./auth/login";
-import HostExperience from "./hostExperience/hostExperience"
-import {BrowserRouter as Router, Route} from "react-router-dom";
+import HostExperience from "./hostExperience/hostExperience";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Register from "./auth/register";
+import HostRegister from "./registration/HostRegister"
+import FeatureStory from "./story/FeatureStory";
+import About from "./about/About";
+import hostGuidelines from "./hostExperience/hostGuidelines";
+import APIExperience from "./api/APIExperience";
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = ({
-            user: null,
-        });
-        this.authListener = this.authListener.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: null
+    };
+    this.authListener = this.authListener.bind(this);
+  }
 
-    componentDidMount() {
-        this.authListener();
-    }
+  componentDidMount() {
+    this.authListener();
+  }
 
-    authListener() {
-        firebase.auth().onAuthStateChanged((user) => {
-            console.log(user);
-            if (user) {
-                this.setState({user});
-                localStorage.setItem('user', user.uid);
-            } else {
-                this.setState({user: null});
-                localStorage.removeItem('user');
-            }
-        });
-    }
+  authListener() {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.setState({ user });
+        console.log(APIExperience.getAllExperiences());
+        localStorage.setItem("user", user.uid);
+      } else {
+        this.setState({ user: null });
+        localStorage.removeItem("user");
+      }
+    });
+  }
 
-
-    render() {
-        return (
-            <div>
-                {this.state.user ? (
-            <Router>
-                    <Route
-                        path="/Explore"
-                        exact
-                        render={() => {
-                            return (
-                                <Explore/>
-                            );
-                        }}
-                    />
-                    <Route
-                        path="/hostexperience"
-                        exact
-                        render={() => {
-                            return (
-                                <HostExperience/>
-                            );
-                        }}
-                    />
-                    <Route
-                        path="/"
-                        exact
-                        render={() => {
-                            return (
-                                <Homepage/>
-                            );
-                        }}
-                    />
-            </Router>): (<Login/>)}</div>
-        );
-    }
+  render() {
+    return (
+      <Router>
+        <Route
+          path="/explore"
+          exact
+          render={() => {
+            return <Explore />;
+          }}
+        />
+        <Route
+          path="/hostexperience"
+          exact
+          render={() => {
+            return <HostExperience />;
+          }}
+        />
+        <Route
+          path="/hostguidelines"
+          exact
+          render={() => {
+            return <hostGuidelines />;
+          }}
+        />
+        <Route
+          path="/register"
+          exact
+          render={() => {
+            return <Register />;
+          }}
+        />
+        <Route
+          path="/about"
+          exact
+          render={() => {
+            return <About />;
+          }}
+        />
+        <Route
+          path="/login"
+          exact
+          render={() => {
+            return <Login />;
+          }}
+        />
+        <Route
+          path="/story"
+          exact
+          render={() => {
+            return <FeatureStory />;
+          }}
+        />
+        <Route
+          path="/hostregister"
+          exact
+          render={() => {
+            return <HostRegister />;
+          }}
+        />
+        <Route
+          path="/"
+          exact
+          render={() => {
+            return <Homepage />;
+          }}
+        />
+      </Router>
+    );
+  }
 }
 
 export default App;
