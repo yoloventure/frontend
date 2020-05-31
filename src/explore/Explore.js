e0101a2de956244f26f9d14fbf5489902fc29b8a
AbdulMoidM <53446831+AbdulMoidM@users.noreply.github.com>
2020-05-31 01:33:23 -0400

    datapicker and industryFilter added in explore

--- src/explore/Explore.js
+++ src/explore/Explore.js
@@ -12,8 +12,17 @@ import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
 export default class Explore extends React.Component{
+  // componentDidMount() {
+  //   this._isMounted=true
+  //   this.setState
+  // }
+  // componentWillUnmount() {
+  //   this._isMounted=false
+  //
+  // }
   constructor(props) {
     super(props)
+    this._isMounted=false
     let  Cards=[];
     this.state = {
       Cards:Cards,
@@ -23,7 +32,7 @@ export default class Explore extends React.Component{
       startDate: new Date()
     }
     this.handleChange = this.handleChange.bind(this);
-
+    this.filterIndustry = this.filterIndustry.bind(this);
 
     for(var i=0; i<data.length;i+=2){
         if(i+1<data.length){
@@ -61,7 +70,80 @@ export default class Explore extends React.Component{
     startDate: date
   });
   };
+  // displayCards(e){
+  //     // Prevent button click from submitting form
+  //    e.preventDefault();
+  //
+  //    // Create variables for our list, the item to add, and our form
+  //    let Cards = this.state.Cards;
+  //    const newItem = document.getElementById("addInput");
+  //    const form = document.getElementById("addItemForm");
+  //
+  //    // If our input has a value
+  //    if (newItem.value != "") {
+  //      // Add the new item to the end of our list array
+  //      list.push(newItem.value);
+  //      // Then we use that to set the state for list
+  //      this.setState({
+  //        list: list
+  //      });
+  //      // Finally, we need to reset the form
+  //      newItem.classList.remove("is-danger");
+  //      form.reset();
+  //    } else {
+  //      // If the input doesn't have a value, make the border red since it's required
+  //      newItem.classList.add("is-danger");
+  //    }
+  // }
 
+  filterIndustry(e){
+    //Prevent button click from submitting form
+    //  e.preventDefault();
+      console.log("i was called")
+      // Create variables for our list, the item to add, and our form
+
+      const targetId = e.target.id;
+      let newItem=document.getElementById(targetId).innerHTML
+      console.log(typeof(newItem))
+      console.log(data)
+      let filteredData=(data.filter(dataElement => dataElement.industry.includes(newItem)))
+      let Cards2=[]
+      for(var i=0; i<filteredData.length;i+=2){
+          if(i+1<filteredData.length){
+          Cards2.push(
+          <div className="row " >
+
+          <div className="card col-lg-4 col-sm-6" style={{padding:'2%'}}>
+          <Card image={filteredData[i].image} id={filteredData[i].id} location={filteredData[i].location} profession={filteredData[i].profession} price={filteredData[i].price} duration={filteredData[i].duration}/>
+          {console.log(filteredData[i].image)}
+          </div>
+
+
+          <div className="card col-lg-4 col-sm-6 offset-1 " style={{padding:'2%'}}>
+          <Card image={filteredData[i+1].image} id={filteredData[i+1].id} location={filteredData[i+1].location} profession={filteredData[i+1].profession} price={filteredData[i+1].price} duration={filteredData[i+1].duration}/>
+          </div>
+
+          </div>);
+
+          }else{
+            Cards.push(
+            <div className="row ">
+            <div className="card col ">
+            <div className=''>
+            <Card image={filteredData[i].image} id={filteredData[i].id} location={filteredData[i].location} profession={filteredData[i].profession} price={filteredData[i].price} duration={filteredData[i].duration}/>
+            </div>
+            </div>
+
+            </div>)
+          }
+      }
+          console.log(Cards2)
+        // Then we use that to set the state for list
+        this.setState({
+          Cards: Cards2
+        });
+
+  }
 
   render(){
 
@@ -71,14 +153,14 @@ export default class Explore extends React.Component{
         <Navbar textColor={'black'} auth={this.props.auth}/>
 
 
-          <div className="row mt-3 pt-5">
-                <div className="col-8">
+          <div className=" row mt-3 pt-5">
+                <div className=" searchArea col-12" >
                     <div className="search">
                         <h1 className="pl-5 pt-4" style={{color:"white", fontSize:"200%"}}>Start Your next Journey </h1>
                         <div class="input-group pt-2">
-                            <input type="text" class="form-control" placeholder="Search"/>
+                            <input id="addInput" type="text" class="form-control" placeholder="Search"/>
                             <div class="input-group-append">
-                              <button class="btn btn-secondary" type="button">
+                              <button class="btn btn-secondary" type="button" onClick="this.displayCards">
                                 <i class="fa fa-search"></i>
                               </button>
                             </div>
@@ -90,8 +172,8 @@ export default class Explore extends React.Component{
                                       Sort By Price
                                     </button>
                                     <div class="dropdown-menu" id="dd1">
-                                        <a className='dropdown-item' onclick='filterIndustry(event)'> Low to High </a>
-                                        <a className='dropdown-item' onclick='filterIndustry(event)'> High to Low </a>
+                                        <a className='dropdown-item' onClick='this.filterIndustry'> Low to High </a>
+                                        <a className='dropdown-item' onClick='this.filterIndustry'> High to Low </a>
 
 
                                     </div>
@@ -103,11 +185,11 @@ export default class Explore extends React.Component{
                                     Filter By Industry
                                   </button>
                                   <div class="dropdown-menu" id="dd1">
-                                      <a className='dropdown-item' onclick='filterIndustry(event)'> Education </a>
-                                      <a className='dropdown-item' onclick='filterIndustry(event)'> Engineering </a>
-                                      <a className='dropdown-item' onclick='filterIndustry(event)'> Art & Design </a>
-                                      <a className='dropdown-item' onclick='filterIndustry(event)'> Healthcare </a>
-                                      <a className='dropdown-item' onclick='filterIndustry(event)'> Food </a>
+                                      <a id='i1' className='dropdown-item' onClick={this.filterIndustry}>Education</a>
+                                      <a id='i2' className='dropdown-item' onClick={this.filterIndustry}>Engineering</a>
+                                      <a id='i3' className='dropdown-item' onClick={this.filterIndustry}>Art & Design</a>
+                                      <a id='i4' className='dropdown-item' onClick={this.filterIndustry}>Healthcare</a>
+                                      <a id='i5' className='dropdown-item' onClick={this.filterIndustry}>Food</a>
 
                                   </div>
                                 </div>
@@ -156,13 +238,14 @@ export default class Explore extends React.Component{
 
 
                             </div>
+
                     </div>
-                    <div style={{paddingLeft:'10%'}}>
-                    {this.state.Cards}
-                    </div>
+
                 </div>
-                <div className='col-4'>
-                  <img src={mapImage} />
+
+
+                <div className='pt-5' style={{paddingLeft:'10%'}}>
+                {this.state.Cards}
                 </div>
 
           </div>
