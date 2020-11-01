dbb2301d05911d048cc01b1ebd91f31a091e4699
AbdulMoidM <53446831+AbdulMoidM@users.noreply.github.com>
2020-11-01 01:23:05 -0400

    'added-new-dev-scripts;

--- client/src/api/apiHost.js
+++ client/src/api/apiHost.js
@@ -14,15 +14,15 @@ function getHostById(hostId){
 }
 
 //returns the infomations about the current Host or null if not logged in
-function getCurrentHost(){
-  var user = getCurrentUser();
-  var hostId = user.hostId;
-  if (hostId != null) {
-    return getHostById(hostId);
-  } else {
-    return null;
-  }
-}
+// function getCurrentHost(){
+//   var user = getCurrentUser();
+//   var hostId = user.hostId;
+//   if (hostId != null) {
+//     return getHostById(hostId);
+//   } else {
+//     return null;
+//   }
+// }
 
 //returns the infomations about all the hosts in the system
 function getAllHosts(){
@@ -92,4 +92,4 @@ function deleteHost(hostId) {
 
 
 
-export default {getHostById, getCurrentHost, getAllHosts, deleteHost, editHost, createNewHost}
+export default {getHostById, getAllHosts, deleteHost, editHost, createNewHost}
