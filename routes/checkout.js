511256da4efd5c1aadb1d9fcca99debad2518409
Tianyi <inipop.tianyi@gmail.com>
2021-11-06 11:44:14 -0400

    checkout first version

--- routes/checkout.js
+++ routes/checkout.js
@@ -4,21 +4,40 @@ const app = express();
 app.use(express.static('public'));
 const router = express.Router();
 
-app.post('/', async (req, res) => {
+let domain = "http://localhost:3000/"
+
+async function getPrice(){
+  const product = await stripe.products.create({name: 'experience'});
+
+  const price = await stripe.prices.create({
+    product: product.id,
+    unit_amount: 100,
+    currency: 'usd',
+  });
+
+  return price
+}
+
+price = getPrice()
+
+router.post('/', async (req, res) => {
   const session = await stripe.checkout.sessions.create({
     line_items: [
       {
         // Price ID to be determined after creating it on dashboard
-        price: 100,
+        price: price.id,
         quantity: 1,
+        currency: 'usd',
+        amount: 100,
+        name: "experience",
       },
     ],
     payment_method_types: [
       'card',
     ],
     mode: 'payment',
-    success_url: `/checkoutsuccess`,
-    cancel_url: `/checkoutcancel`,
+    success_url: domain + `checkoutsuccess`,
+    cancel_url: domain + `checkoutcancel`,
   });
 
   res.redirect(303, session.url)
